0862fd620b78900a4e36467190781f3142b6278f
venkatakhileshwar <51919589+venkatakhileshwar@users.noreply.github.com>
2019-08-19 03:15:26 -0400

    updated php code in login page

--- OnlineApp/login.php
+++ OnlineApp/login.php
@@ -1,2 +1,27 @@
-login.php
-<?
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+
+
+
+$con = mysqli_connect("localhost","root","","OABS");
+// Check connection
+
+ $query = "select * from login where username='$username' and password= '$password'";
+
+$result = mysqli_query($con,$query) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
+        if($rows==1){
+     
+           echo "success";
+           header("Location: Home page.htm");
+    
+         }
+
+else{
+	echo "Failed";
+}
+?>
